c2f220c948e616bc872205282dbdf6129b778bc3
Alex Maina <lexizgichbuoy@gmail.com>
2024-06-03 14:35:30 +0300

    update cv

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -17,7 +17,7 @@ const Homepage = () => {
   const handleDownloadCV = () => {
     // Implement your download CV logic here
     // use window.open() to download the CV file
-    window.open("https://drive.google.com/file/d/17Kc137rqghQ-Pc6H4EalanOx93kJC0-J/view?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/1ZTr34IGnivyuogPF_GBXfQhjpnhvzR9y/view?usp=sharing", "_blank");
   };
 
   const handleGithubClick = () => {
